5ba2086853634c6bdec88963dc72afde7a94bd98
natanf2051 <115187503+natanf2051@users.noreply.github.com>
2023-03-10 17:21:02 -0300

    .

--- painel-de-vendas/src/main/java/com/painel/domain/cliente/ClientesRepository.java
+++ painel-de-vendas/src/main/java/com/painel/domain/cliente/ClientesRepository.java
@@ -2,13 +2,14 @@ package com.painel.domain.cliente;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 
 public interface ClientesRepository extends JpaRepository<Clientes, String> {
 
-  
-   Clientes findByCpf(String cpf);
+  @Query(value = "SELECT id, nome, cpf FROM clientes where cpf = ':cpf'")
+  Clientes findByCpf(String cpf);
     
 
    //@Query("SELECT c.nome, c.telefone FROM Clientes c WHERE c.cpf = :cpf")
